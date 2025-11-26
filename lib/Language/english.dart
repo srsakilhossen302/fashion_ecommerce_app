b6f036dc3b2eaf4600f934e2dc43586b5abe7951
srsakilhossen302 <cse.sakilhossem@gmail.com>
2025-11-26 10:23:18 +0600

    translator to english

---
+++ lib/Language/english.dart
@@ -0,0 +1,120 @@
+import 'package:e_commerce_fashion_app/Utils/StaticString/static_string.dart';
+
+Map<String, String> english = {
+  ///============== Sign In View ==============///
+  AppString.welcome: "Welcome Back!",
+  AppString.UsernameOrEmail: "Username or Email",
+  AppString.password: "Password",
+  AppString.forgotPassword: "Forgot password?",
+  AppString.loginB: "Login",
+  AppString.continueWith: "- OR Continue with -",
+  AppString.createAnAccount: "Create An Account",
+  AppString.signUp: "Sign Up",
+
+  ///============== Sign Up View ==============///
+  AppString.confirmPassword: "Confirm Password",
+  AppString.byClicking: "By clicking the",
+  AppString.SingUpB: "Register",
+  AppString.publicOffer: "button, you agree to the public offer",
+  AppString.createAccount: "Create Account",
+  AppString.alreadyAccount: "I Already Have an Account",
+
+  ///============== T-Shirt Explore More ==============///
+  AppString.moreTShirt: "6500+ T_Shirt",
+  AppString.newI: "New",
+  AppString.women: "Women",
+  AppString.allTShirt: "All T_Shirt",
+  AppString.tShirtTitle: "21WN reversible angora cardigan",
+  AppString.tShirtDescription: "reversible angora cardigan",
+
+  ///============== Home Page ==============///
+  AppString.luxury: "LUXURY",
+  AppString.fashion: "FASHION",
+  AppString.accessories: "& ACCESSORIES",
+  AppString.exploreCollection: "Explore Collection",
+  AppString.newArrival: "New Arrival",
+  AppString.all: "All",
+  AppString.apparel: "Apparel",
+  AppString.dress: "Dress",
+  AppString.tShirt: "T-shirt",
+  AppString.bag: "Bag",
+  AppString.collections: "Collections",
+  AppString.autumn: "Autumn",
+  AppString.collection: "COLLECTION",
+  AppString.justForYou: "Just for You",
+  AppString.hTitle: "21WN reversible angora cardigan",
+  AppString.hDescription: "reversible angora cardigan",
+  AppString.trending: "@Trending",
+  AppString.sal: "#2021",
+  AppString.spring: "#spring",
+  AppString.sCollection: "#collection",
+  AppString.fall: "#fall",
+  AppString.sDress: "#dress",
+  AppString.autumnCollection: "#autumncollection",
+  AppString.openFashion: "#openfashion",
+  AppString.dailyDrive: "daily drive.",
+  AppString.making:
+      "Making a luxurious lifestyle accessible for a generous group of women is our",
+  AppString.followUs: "Follow Us",
+  AppString.supportSakil: "support@Sakil.design",
+  AppString.phnNumbar: "+8801774452302",
+  AppString.time: "08:00 - 22:00 - Everyday",
+  AppString.about: "About",
+  AppString.contact: "Contact",
+  AppString.blog: "Blog",
+  AppString.exploreMore: "Explore More",
+  AppString.copyright: "Copyright © 2025 Sakil. All Rights Reserved.",
+
+  ///============== Product Details Page ==============///
+  AppString.checkout: "Checkout",
+  AppString.addPromoCode: "Add promo code",
+  AppString.delivery: "Delivery",
+  AppString.free: "Free",
+  AppString.estTotal: "Est. Total",
+
+  ///============== Dress Explore More ==============///
+  AppString.d650000All: "650000+ All",
+  AppString.allFashion: "All Fashion",
+  AppString.DressTitle: "21WN reversible angora cardigan",
+  AppString.DressDescription: "reversible angora cardigan",
+
+  ///============== Promo Page ==============///
+  AppString.promo: "Promo",
+  AppString.policy: "Policy",
+  AppString.lookbook: "Lookbook",
+  AppString.sale: "Sale",
+  AppString.sans: "Sans",
+  AppString.load: "Load more",
+  AppString.promoDate: "11/10/2025",
+  AppString.promoTitle: "2021 Style Guide: The Biggest Fall Trends",
+  AppString.promoDescription:
+      "The excitement of fall fashion is here and I’m already loving some of the trend forecasts "
+      "You guys know how much I love mixing high and low-end – it’s the best way to get the most bang for "
+      "your buck while still elevating your wardrobe. The same goes for handbags! And honestly they "
+      "are probably the best pieces to mix and match. I truly think the key to completing a look is with "
+      "a great bag and I found so many this year that I wanted to share a round-up of my most worn handbags.",
+  AppString.cardText:
+      "I found this Saint Laurent canvas handbag this summer and immediately fell in love. "
+      "The neutral fabrics are so beautiful and I like how this handbag can also carry into fall. "
+      "The mini Fendi bucket bag with the sheer fabric is so fun and such a statement bag. "
+      "Also this DeMellier off white bag is so cute to carry to a dinner with you or going out, "
+      "it’s small but not too small to fit your phone and keys still.",
+  AppString.posted: "Posted by OpenFashion |",
+  AppString.postedDate: "3 Days ago",
+  AppString.postedFashion: "#Fashion",
+  AppString.postedTips: "#Tips",
+
+  ///============== Blog Grid ==============///
+  AppString.blogTitle: "2021 Style Guide: The Biggest Fall Trends",
+  AppString.blogDescription:
+      "You guys know how much I love mixing high and low-end – it’s the best way to get the most bang for your buck.",
+  AppString.blogDate: "4 days ago",
+  AppString.loadMore: "Load more",
+
+  AppString.blogGridDate: "15/10/2005",
+  AppString.blogGridTitle: "2021 Style Guide: The Biggest Fall Trends",
+  AppString.blogGridDescription:
+      "The excitement of fall fashion is here and I’m already loving some of the trend forecasts "
+      "You guys know how much I love mixing high and low-end – it’s the best way to elevate your wardrobe. "
+      "Handbags are the best pieces to mix and match, and I wanted to share a round-up of my most worn handbags.",
+};
